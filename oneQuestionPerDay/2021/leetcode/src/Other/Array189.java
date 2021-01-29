54a37397e6963f3d2fd9c5b181d97e267b3fea79
kddk <eeemail1217@gmail.com>
2021-01-29 21:18:57 +0800

    1.29更新：完善题目名称，新增3题题解，新增题目难度

---
+++ oneQuestionPerDay/2021/leetcode/src/Other/Array189.java
@@ -0,0 +1,63 @@
+package Other;
+
+/**旋转数组  难度：medium
+ *
+ *给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
+ *
+ * 示例 1:
+ * 输入: nums = [1,2,3,4,5,6,7], k = 3
+ * 输出: [5,6,7,1,2,3,4]
+ * 解释:
+ * 向右旋转 1 步: [7,1,2,3,4,5,6]
+ * 向右旋转 2 步: [6,7,1,2,3,4,5]
+ * 向右旋转 3 步: [5,6,7,1,2,3,4]
+ *
+ * 示例 2:
+ * 输入：nums = [-1,-100,3,99], k = 2
+ * 输出：[3,99,-1,-100]
+ * 解释:
+ * 向右旋转 1 步: [99,-1,-100,3]
+ * 向右旋转 2 步: [3,99,-1,-100]
+ *  
+ * 提示：
+ * 1 <= nums.length <= 2 * 104
+ * -231 <= nums[i] <= 231 - 1
+ * 0 <= k <= 105
+ */
+
+/**
+ * 思路：翻转数组
+ *
+ *             nums = "----->-->"; k =3
+ *             result = "-->----->";
+ *
+ *             reverse "----->-->" we can get "<--<-----"
+ *             reverse "<--" we can get "--><-----"
+ *             reverse "<-----" we can get "-->----->"
+ *
+ *
+ */
+public class Array189 {
+    public void rotate(int[] nums, int k) {
+
+        //若没有取余操作，则会数组越界，原因是k的值大于数组长度。
+        k %= nums.length;
+        //翻转整个数组
+        reverse(nums, 0, nums.length - 1);
+        //翻转前k个元素
+        reverse(nums, 0, k - 1);
+        //翻转剩下的元素
+        reverse(nums, k, nums.length - 1);
+    }
+
+    //翻转方法
+    public void reverse(int[] nums, int start, int end){
+        while(start < end){
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
---
+++ oneQuestionPerDay/2021/leetcode/src/Other/HashSet217.java
@@ -0,0 +1,39 @@
+package Other;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**存在重复元素  难度：easy
+ *
+ * 给定一个整数数组，判断是否存在重复元素。
+ *
+ * 如果存在一值在数组中出现至少两次，函数返回 true 。如果数组中每个元素都不相同，则返回 false 。
+ *
+ * 示例 1:
+ * 输入: [1,2,3,1]
+ * 输出: true
+ *
+ * 示例 2:
+ * 输入: [1,2,3,4]
+ * 输出: false
+ * 示例 3:
+ *
+ * 输入: [1,1,1,3,3,4,3,2,4,2]
+ * 输出: true
+ */
+
+/**
+ * 思路：对于数组中每个元素，我们将它插入到哈希表中。如果插入一个元素时发现该元素已经存在于哈希表中，则说明存在重复的元素。
+ */
+public class HashSet217 {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> res = new HashSet<>();
+        for(int num : nums){
+            if(res.contains(num)){
+                return true;
+            }
+            res.add(num);
+        }
+        return false;
+    }
+}
